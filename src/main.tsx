aba7e6bd2188af4356cb0a128cd704f7e5f62612
Nikhil Rao <nikhil01446@gmail.com>
2026-08-02 17:11:43 +0530

    fix: remove unused shadcn card component causing css minification build errors; set default theme to light

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from 'next-themes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
       <App />
     </ThemeProvider>
   </StrictMode>,
